644aa87b9c59e27d6d067e6193bfcd4bbb67ad79
Charles Uthulor <charlesnnanna1667@gmail.com>
2021-03-23 10:24:01 -0700

    Fetch authentication data from server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route} from 'react-router-dom'
+import Header from './Header'
+
+
+const Landing = () => <h1>Landing</h1>
+const Dashboard = () => <h1>Dashboard</h1>
+const SurveyNew = () => <h1>SurveyNew</h1>
+
 
 const App = () => {
     return(
-        <div>
-            Hi there Charles
+        <div className = 'container'>
+            <BrowserRouter>
+                <Header/>
+                <Route exact path = '/' component = {Landing} />
+                <Route exact path = '/surveys' component = {Dashboard} />
+                <Route path = '/surveys/new' component = {SurveyNew} />
+            </BrowserRouter>
         </div>
     )
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,11 @@ module.exports = (app) => {
 
     app.get('/auth/google/callback', passport.authenticate('google'))
 
-    app.get('/logout', (req, res) => {
+    app.get('/api/logout', (req, res) => {
         req.logout();
         res.send('You have been logged out')
     })
-    app.get('/user', (req, res) => {
+    app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     })
 
